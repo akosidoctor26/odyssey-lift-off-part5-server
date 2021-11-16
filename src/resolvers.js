1f3067f774a4e41c916e3252f8460f084c86c171
Kristine Doc <mskristinedoctor@gmail.com>
2021-11-16 07:32:51 -0700

    @deprecated

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,6 +44,14 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+
+    /* Track.author and Track.modules resolvers */
+    durationInSeconds: ({ length }) => length,
+  },
+
+  Module: {
+    /* Track.author and Track.modules resolvers */
+    durationInSeconds: ({ length }) => length,
   },
 };
 
